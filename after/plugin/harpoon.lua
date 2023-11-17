9f91972eacced11b02a81a26d534971085740e8e
Carlos Roberto <crpu4a@gmail.com>
2023-11-17 10:22:24 -0300

    feat: change harpoon comands

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,9 +1,9 @@
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
-vim.keymap.set('n', '<leader>h', mark.add_file)
-vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+vim.keymap.set('n', '<leader>hh', mark.add_file)
+vim.keymap.set('n', '<C-h>', ui.toggle_quick_menu)
 
-vim.keymap.set('n', '<C-h>', function () ui.nav_file(1) end)
-vim.keymap.set('n', '<C-t>', function () ui.nav_file(2) end)
-vim.keymap.set('n', '<C-g>', function () ui.nav_file(3) end)
+vim.keymap.set('n', '<leader>h1', function () ui.nav_file(1) end)
+vim.keymap.set('n', '<leader>h2', function () ui.nav_file(2) end)
+vim.keymap.set('n', '<leader>h3', function () ui.nav_file(3) end)
